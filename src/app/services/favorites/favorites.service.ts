b69db40ac297ba15a38bcca551f602088382ae8f
mateuszhadrian <tenhadrian@gmail.com>
2026-06-10 12:40:28 +0200

    refactor(favorites): extract localStorage read/write helpers

--- src/app/services/favorites/favorites.service.ts
+++ src/app/services/favorites/favorites.service.ts
@@ -4,24 +4,34 @@ import { Photo } from '../../interfaces/photo.interface';
 @Service()
 export class FavoritesService {
   private static readonly localStorageKey = 'favorites';
-  private readonly _favorites = signal<Photo[]>(
-    JSON.parse(localStorage.getItem(FavoritesService.localStorageKey) ?? '[]'),
-  );
+  private readonly _favorites = signal<Photo[]>(this.getFavoritesFromLocalStorage());
 
   readonly favorites = this._favorites.asReadonly();
 
   add(photo: Photo) {
     if (this.isFavorite(photo.id)) return;
     this._favorites.update((favorites) => [...favorites, photo]);
-    localStorage.setItem(FavoritesService.localStorageKey, JSON.stringify(this._favorites()));
+    this.setInLocalStorage();
   }
 
   remove(photoId: string) {
     this._favorites.update((favorites) => favorites.filter((favorite) => favorite.id !== photoId));
-    localStorage.setItem(FavoritesService.localStorageKey, JSON.stringify(this._favorites()));
+    this.setInLocalStorage();
   }
 
   isFavorite(photoId: string): boolean {
     return this._favorites().some((photo) => photo.id === photoId);
+  }
+
+  private setInLocalStorage() {
+    localStorage.setItem(FavoritesService.localStorageKey, JSON.stringify(this._favorites()));
+  }
+
+  private getFavoritesFromLocalStorage() {
+    try {
+      return JSON.parse(localStorage.getItem(FavoritesService.localStorageKey) ?? '[]');
+    } catch {
+      return [];
+    }
   }
 }
